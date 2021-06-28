f28f4fde699c3935e51947d3f75ed5183656e183
VladimirNov <remixxx998@mail.ru>
2021-06-28 18:55:41 +0300

    Lab9

---
+++ skills.js
@@ -0,0 +1,26 @@
+const skills = [
+    {css_class: "skill-html", name: "HTML", level: 60},
+    {css_class: "skill-csharp", name: "C#", level: 65},
+    {css_class: "skill-css", name: "CSS", level: 40},
+    {css_class: "skill-1c", name: "1C", level: 29},
+]
+function insert_skills() {
+    const dl = document.createElement('dl');
+    dl.classList.add('skills-list');
+    skills.forEach(item => {
+        const dt = document.createElement('dt');
+        dt.textContent = item.name;
+        dt.classList.add(item.css_class);
+        const dd = document.createElement('dd');
+        dd.classList.add('level');
+        const div = document.createElement('div');
+        div.style.width = item.level + '%';
+        div.textContent = item.level;
+        dd.appendChild(div);
+        dl.append(dt);
+        dl.append(dd);
+    });
+    document.getElementById('skills').append(dl)
+}
+
+insert_skills();
